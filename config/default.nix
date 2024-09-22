949844e867542550717079cf923ad2acdb3105e5
Lunics <git@lunics.fr>
2024-09-22 22:45:39 +0200

    init nixvim
    Add options

---
+++ config/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./options.nix
+    ./bufferline.nix
+  ];
+}
--- flake.nix
+++ flake.nix
@@ -1,35 +1,42 @@
 {
   description = "Nixvim configuration";
 
-  inputs.nixvim.url = "github:nix-community/nixvim";
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim.url      = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts"; };
 
-  outputs = {
-    self,
-    nixvim,
-    flake-parts,
-  } @ inputs: let
-    config = {
-      colorschemes.gruvbox.enable = true;
-    };
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    { nixvim, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "aarch64-linux"
         "x86_64-linux"
-      ];
+        "aarch64-linux" ];
 
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        nixvim' = nixvim.legacyPackages."${system}";
-        nvim = nixvim'.makeNixvim config;
-      in {
-        packages = {
-          inherit nvim;
-          default = nvim;
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit pkgs;
+            module = import ./config; # import the module directly
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
+        {
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+          };
+
+          packages = {
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
+          };
         };
-      };
     };
 }
