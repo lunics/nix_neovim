8382af4270b26c8ea9a5dbb9b0cef1f88e3a6f8f
Lunics <git@lunics.fr>
2024-09-24 20:24:10 +0200

    Add highlight-undo.nvim plugin

--- plugins/default.nix
+++ plugins/default.nix
@@ -27,16 +27,16 @@
   extraPlugins = with pkgs.vimPlugins; [
     # nix-env -i nix-prefetch
     # nix-prefetch https://github.com/tzachar/highlight-undo.nvim
-    (pkgs.vimUtils.buildVimPlugin {
-      pname    = "highlight-undo.nvim";
-      version  = "2024-05-06";
-      src = pkgs.fetchFromGitHub {
-        owner  = "tzachar";
-        repo   = "highlight-undo.nvim";
-        rev    = "1ea1c79372d7d93c88fd97543880927b7635e3d2";
-        sha256 = "1lzml5yj3ak0rc4r2fmb1zpb9a01i5j6yk58m0qn960l2x5kav68";
-      };
-    })
+    {
+      plugin = (pkgs.vimUtils.buildVimPlugin {
+        pname    = "highlight-undo.nvim";
+        version  = "2024-05-06";
+        src = pkgs.fetchFromGitHub {
+          owner  = "tzachar";
+          repo   = "highlight-undo.nvim";
+          rev    = "1ea1c79372d7d93c88fd97543880927b7635e3d2";
+          sha256 = "1lzml5yj3ak0rc4r2fmb1zpb9a01i5j6yk58m0qn960l2x5kav68"; }; });
+    }
   ];
   #  vim-nix
   #  {
