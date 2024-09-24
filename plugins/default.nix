82bf3ea320fbe7fe700e1f2b47c59ada5b42f706
Lunics <git@lunics.fr>
2024-09-24 20:26:06 +0200

    Add highlight-undo.nvim plugin

--- plugins/default.nix
+++ plugins/default.nix
@@ -36,6 +36,25 @@
           repo   = "highlight-undo.nvim";
           rev    = "1ea1c79372d7d93c88fd97543880927b7635e3d2";
           sha256 = "1lzml5yj3ak0rc4r2fmb1zpb9a01i5j6yk58m0qn960l2x5kav68"; }; });
+      config = ''
+        require('highlight-undo').setup({
+        duration = 300,
+        undo = {
+          hlgroup = 'HighlightUndo',
+          mode = 'n',
+          lhs = 'u',
+          map = 'undo',
+          opts = {}
+        },
+        redo = {
+          hlgroup = 'HighlightRedo',
+          mode = 'n',
+          lhs = '<C-r>',
+          map = 'redo',
+          opts = {}
+        },
+        highlight_for_count = true,
+      })'';
     }
   ];
   #  vim-nix
