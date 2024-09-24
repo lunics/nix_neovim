45eefb7d239381cb1f3c92eef83b5feb40a01171
Lunics <git@lunics.fr>
2024-09-24 21:32:47 +0200

    add autocmd highlight_yank.nix

--- autocmds/default.nix
+++ autocmds/default.nix
@@ -1,7 +1,9 @@
 {
   imports = [
-    ./exit_cursor.nix
-    ./save_cursor_pos.nix
     ./disable_auto_comment.nix
+    ./exit_cursor.nix
+    ./header_file_ext.nix
+    ./highlight_yank.nix
+    ./save_cursor_pos.nix
   ];
 }
