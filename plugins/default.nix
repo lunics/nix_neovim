45ff34fe912254d6e59bd8b9c0bc91791763073f
Lunics <git@lunics.fr>
2024-10-26 19:45:16 +0200

    Clean default plugins

--- plugins/default.nix
+++ plugins/default.nix
@@ -14,6 +14,14 @@
     # ./trim.nix
   ];
 
+  plugins = {
+    commentary.enable     = true;
+    auto-save.enable      = true;
+    nvim-surround.enable  = true;
+    spider.enable         = true;   # ?
+    numbertoggle.enable   = true;   # turn off relative number when buffer not focused
+  };
+
   # { 'nvim-tree/nvim-web-devicons', },
   # { 'tpope/vim-abolish', },
   # { 'svban/YankAssassin.vim', },
@@ -26,35 +34,5 @@
   #   treesitter
   # https://github.com/akinsho/git-conflict.nvim/
   # folke/which-key.nvim',
-
-  plugins = {
-    commentary.enable     = true;
-    auto-save.enable      = true;
-    nvim-surround.enable  = true;
-    spider.enable         = true;
-    # toggleterm.nvim   manage multiple terminal windows
-    numbertoggle.enable = true;     # turn off relative number when buffer not focused
-  };
-
-  # extraPackages = with pkgs; [
-  #   marksman
-  #   (nerdfonts.override {
-  #     fonts = [
-  #       "JetBrainsMono"
-  #       "RobotoMono"
-  #     ];
-  #   })
-  # ];
-
-  # Not all plugins have modules, so install the others here
-  # extraPlugins = with pkgs.vimPlugins; [
-  # ];
-  #  vim-nix
-  #  {
-  #    plugin = commen-nvim
-  #    config = ''lua require("Comment").setup()'';
-  #  }
-  #];
-  # or this for the configuration
-  # extraConfigLua = "require('my-plugin').setup({foo = "bar"})";
+  # toggleterm.nvim   manage multiple terminal windows
 }
